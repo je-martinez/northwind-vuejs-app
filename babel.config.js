10a591d6f591a7cf180b80e9575840b5ddefcb8c
Jose E. Martinez <josemartinezchevez@gmail.com>
2024-08-25 22:29:41 -0600

    chore: babel plugin to remove unused console logs

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,10 @@
 const plugins = [];
 
-if (process.env.NODE_ENV == "staging" || process.env.NODE_ENV == "production") {
+if (process.env.NODE_ENV == "production") {
   plugins.push("transform-remove-console");
 }
 
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset"],
-  plugins,
+  plugins: plugins,
 };
